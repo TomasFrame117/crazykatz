7510838cbe221dc12faa8a041b1d19b6cbc5bb87
Tomas Nøklegaard <88710155+TomasFrame117@users.noreply.github.com>
2022-03-17 14:54:26 +0100

    submit changes
    view update

--- model.js
+++ model.js
@@ -1,9 +1,10 @@
 
 let model = {
-    //app    
+    //app  
+    app:{  
     currentPage: '',
     currentUser: null,
-
+    },
 
     //input
     input: {
@@ -41,6 +42,7 @@ let model = {
         rateColor: 0,
         rateEars: 0,
         rateOverall: 0,
+        pointTotal: 100,
             
     },
 
@@ -56,7 +58,7 @@ let model = {
             startTime:'12:00',
             endTime:'20:00',
             price: 123,
-            allDay: false
+            allDay: false,
         }, // putte in info som trengs
         {
             id: 2,
@@ -102,7 +104,7 @@ let model = {
     ],
 
     usersCatPictures:[
-        {personId:1,imgurl:"img1.png",ratings:[{fur:3, head:4, tail:4, ears:5, color:6, overall:50},{fur:3, head:4, tail:4, ears:5, color:6, overall:50},],},
+        {personId:1,imgurl:"img1.png",ratings:[{fur:3, head:4, tail:4, ears:5, color:6, overall:50}, {fur:3, head:4, tail:4, ears:5, color:6, overall:50},],},
         {personId:2,imgurl:"img1.png",ratings:[{fur:1, head:6, tail:2, ears:3, color:4, overall:35},],},
     ],
 
@@ -111,34 +113,34 @@ let model = {
                     id: 1,
                     name: 'Norsk Skogkatt',
                     info:'Norsk skogkatt (NFO) er en katterase som første gang ble anerkjent av FIFé i 1977, men som har røtter som går langt tilbake i tid. Som rase ble den skapt i Norge på 1930-tallet, men den er trolig mer vanlig i Sverige.',
-                image:["/bilder/skogkatt.jpg"]
+                    image:["/bilder/skogkatt.jpg"]
                 },
                 {
                     id: 2,
                     name: 'Siameser',
                     info:'Siameser er en middels stor, lang, slank katt, med en meget kort, fin pels uten underpels. Halen er lang og avsmalnende. Hodet smalner til snuten i rette linjer med lang rett nese. Øynene er blå.',
-                image:["/bilder/siamesisk.jpg",]
+                    image:["/bilder/siamesisk.jpg",]
                 },
                 {
                     id: 3,
                     name: 'American Curl Langhår',
                     info:'Rasen kommer altså som to raser; langhåret og korthåret. Den langhårede utgaven har ganske lang pels, mens den korthårede har kort eller halvlang pels. Pelsen til begge er myk og lett med lite underull. Pelsen kommer i de fleste farger og mønstre, og en haug ulike kombinasjoner av disse.',
-                image:["/bilder/americancurllong.webp",]},
+                    image:["/bilder/americancurllong.webp",]},
                 {
                     id: 4,
                     name: 'Maine coon',
                     info:'Maine coon er en stor og kraftig katt, den største av tamkattrasene. En fullvoksen hann veier 6–8 kilo, mens hunnen veier 4–6 kilo. Det kan ta flere år før katten er fullt utvokst. Pelsen er mellomlang, silkeaktig og tett.',
-                image:["/bilder/mainecoon.jpeg"]},
+                    image:["/bilder/mainecoon.jpeg"]},
                 {
                     id: 5,
                     name: 'Scottish Fold',
                     info: 'Scottish Fold er en rase som oppstod av en spontan mutasjon i Skottland i 1961. Det var en katt med foldede ører som fødte en hunn katt med navnet Snooks og sammen med en britisk korthår fødte Snooks en hann katt med navn Snowball. Rasen til Snowball fikk navnet ” folded Ear ”.',
-                image:["https://media.istockphoto.com/photos/lovable-scottish-fold-cat-picture-id467801366?k=20&m=467801366&s=612x612&w=0&h=FBdXfK4jt-jRzL8jpFbHHFOkMBobMHsGkpEDiDKCiGs="]},
+                    image:["https://media.istockphoto.com/photos/lovable-scottish-fold-cat-picture-id467801366?k=20&m=467801366&s=612x612&w=0&h=FBdXfK4jt-jRzL8jpFbHHFOkMBobMHsGkpEDiDKCiGs="]},
                 {
-                     id: 6,                                                                                                                                                 
+                    id: 6,                                                                                                                                                 
                     name: 'Ragdoll',
                     info: 'Ragdoll er en lang muskuløs katt med bredt bryst og lang buskete hale. Hodet er middels stort, med fyldige kinn, og lav og flat mellom ørene. Øynene er store, ovale og blå, så vidt skråstilte. Særegent for rasen er dens maskefarger og hvitflekkethet.',
-                image:["https://thumbs.dreamstime.com/b/awesome-ragdoll-cat-beautiful-cushion-adorable-ragdoll-cat-beautiful-cushion-172354412.jpg"]},
+                    image:["https://thumbs.dreamstime.com/b/awesome-ragdoll-cat-beautiful-cushion-adorable-ragdoll-cat-beautiful-cushion-172354412.jpg"]},
 
                 
             
--- view.js
+++ view.js
@@ -1,5 +1,88 @@
-function show(){
-    document.getElementById('app').innerHTML =
-    /*html*/
-     ``;
+//let html = document.getElementById('app');
+    
+
+function show() {
+    document.getElementById('app').innerHTML = `
+    <div>
+        <h1>Crazy Cat People</h1><br/>
+        <input class="log" type="text" placeholder="E-post:"/> <br/>
+        <input class="log" type="password" placeholder="Passord:"/><br/>
+        <button class="in" onclick="homePage()">Log Inn</button></br>
+        <button class="reg" onclick="regPage()">Registrer</button>
+    </div>
+   
+    `;
+    updateView();
+    }
+
+
+
+function homePage() {
+    document.getElementById('app').innerHTML = `
+        <h1>Crazy Katz</h1>
+       <div class="container"> <button class="homeMenu" onclick="homePage()">Hjem</button></br>
+        <button class="homeMenu" onclick="ratingsPage()">Ratings</button></br>
+        <button class="homeMenu" onclick="forumPage()">Katt Forum</button></br>
+        <button class="homeMenu" onclick="infoPage()">Info om katter</button></br>
+        <button class="homeMenu" onclick="humorPage()">Humor</button></br>
+        <button class="homeMenu" onclick="searchPage()">Søk</button></br></div>
+    `;
+    model.app.currentPage = 'homePage';
+    updateView();
 }
+
+function regPage(){
+    document.getElementById('app').innerHTML = 
+    `<h1>Lag ny bruker</h1>
+     <input class="Uname1" type="text" placeholder="Fornavn:"/><br/>
+     <input class="Lname1" type="text" placeholder="Etternavn:"/></br>
+     <input class="UserEm1" type="text" placeholder="E-post:"/></br>
+     <input class="Upass1" type="text" placeholder="Passord:"/></br>
+     <input class="BUpass1" type="text" placeholder="Gjenta Passord:"/></br>
+    <button class="register" onclick="homePage()">Registrer</button></br>
+    `;
+    updateView();
+}
+
+function myProfile(){
+    document.getElementById('app').innerHTML = `
+    <h1>Min profil</h1>
+    <h3 class="prinfo">Profil informasjon</h3>
+    <h3 class="profbilde">Legg til profil bilde</h3>
+    <input class="Uname" type="text" placeholder="Fornavn:"/>
+    <input class="Lname" type="text" placeholder="Etternavn:"/>
+     <input class="UserEm" type="text" placeholder="E-post:"/>
+     <input class="Upass" type="text" placeholder="Passord:"/>
+     <input class="BUpass" type="text" placeholder="Gjenta Passord:"/>
+
+     <button class="edit" onclick="">Rediger</button>
+     <button class="browse" onclick="">Browse</button>
+     <button class="legg" onclick="">Legg til</button>`
+}
+function ratingsPage(){
+    document.getElementById('app').innerHtml='';
+}
+function calendarPage(){
+    document.getElementById('app').innerHtml='';
+}
+function forumPage(){
+    document.getElementById('app').innerHtml='';
+}
+function infoPage(){
+    document.getElementById('app').innerHtml='';
+}
+
+updateView();
+    function updateView() {
+    const side = model.app.currentPage;
+    if (side == 'loginPage') loginPage();
+    if (side == 'regPage') regPage();
+    if (side == 'myProfile') myProfile();
+    if (side == 'homePage') homePage();
+    //together
+    if (side == 'ratingsPage') ratingsPage();   
+    if (side == 'calendarPage') calendarPage();
+    if (side == 'forumPage') forumPage();
+    //christian
+    if (side == 'infoPage') infoPage();
+    } 
